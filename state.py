069297b3e8103902b03149f5266cd4056e2fd4cf
Jmeyer89 <matzahart@gmail.com>
2026-05-07 03:39:05 -0400

    Add panel writer task and edit-pacing state

--- state.py
+++ state.py
@@ -12,6 +12,10 @@ now_playing_tasks: dict[int, asyncio.Task] = {}
 inactivity_tasks: dict[int, asyncio.Task] = {}
 play_locks: dict[int, asyncio.Lock] = {}
 
+panel_writer_tasks: dict[int, asyncio.Task] = {}
+panel_update_events: dict[int, asyncio.Event] = {}
+panel_last_edit_at: dict[int, float] = {}
+
 queue_expanded: dict[int, bool] = {}
 volume_levels: dict[int, float] = {}
 search_alternatives: dict[int, list[dict]] = {}
